340de11394a84eb060382a639ee405bf89935039
Ke Zhao <kezhao@Kes-MacBook-Pro.attlocal.net>
2018-02-10 20:22:54 -0600

    added

---
+++ index.js
@@ -0,0 +1,45 @@
+// Get references to the tbody element, input field and button
+var $tbody = document.querySelector("tbody");
+var $datetimeInput = document.querySelector("#datetime");
+var $searchBtn = document.querySelector("#search");
+
+// Add an event listener to the searchButton, call handleSearchButtonClick when clicked
+$searchBtn.addEventListener("click", handleSearchButtonClick);
+
+// Set filteredAddresses to addressData initially
+var filteredData = dataSet;
+
+// renderTable renders the filteredAddresses to the tbody
+function renderTable() {
+  $tbody.innerHTML = "";
+  for (var i = 0; i < filteredData.length; i++) {
+    // Get get the current address object and its fields
+    var datetime = filteredData[i];
+    var fields = Object.keys(datetime);
+    // Create a new row in the tbody, set the index to be i + startingIndex
+    var $row = $tbody.insertRow(i);
+    for (var j = 0; j < fields.length; j++) {
+      // For every field in the address object, create a new cell at set its inner text to be the current value at the current address's field
+      var field = fields[j];
+      var $cell = $row.insertCell(j);
+      $cell.innerText = datetime[field];
+    }
+  }
+}
+
+function handleSearchButtonClick() {
+  // Format the user's search by removing leading and trailing whitespace, lowercase the string
+  var filterDatetime = $stateInput.value.trim().toLowerCase();
+
+  // Set filteredAddresses to an array of all addresses whose "state" matches the filter
+  filteredData = dataSet.filter(function(datetime) {
+    var dataDatetime = data.datetime.toLowerCase();
+
+    // If true, add the address to the filteredAddresses, otherwise don't add it to filteredAddresses
+    return dataDatetime === filterDatetime;
+  });
+  renderTable();
+}
+
+// Render the table for the first time on page load
+renderTable();
